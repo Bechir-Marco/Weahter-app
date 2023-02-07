dcd4389072b38aeeac7a5f1370b3ad1073fb110c
Bechir-Marco <bechir.marko@gmail.com>
2023-02-07 21:25:38 +0100

    error handling for Geolocation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require("postman-request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=516f5691e6bc38ec80e290fe46ad28f1&query=New%20York";
+  "http://api.weatherstack.com/current?access_key=516f5691e6bc38ec80e290fe46ad28f1&query=New%20York&unit=m";
 
 request({ url: url }, (erro, response) => {
     const data = JSON.parse(response.body)

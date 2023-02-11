da14c219a0bdd21a8cc1c71e9d81133725b1ee0d
Bechir-Marco <bechir.marko@gmail.com>
2023-02-11 22:37:02 +0100

    Web Server Configuration and creating templates with handelbars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,64 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
-
-
-const publicDirectoryPath = path.join(__dirname,'../public');
-const aboutDirectoryPath = path.join(__dirname,'../public/about.html');
-const helpDirectoryPath = path.join(__dirname,'../public/help.html');
 
 const app = express()
 
+//Define paths for Exprexx config 
+const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname, '../templates/views')
+const partials = path.join(__dirname,'../templates/partials')
+
+// Setup handelbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partials)
+// Setup static directory to serve 
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+            title:'Weather App',
+            name: 'Bechir Marco'
+
+        })
+    })
+app.get('/about', (req, res) => {
+    res.render('about', {
+            title:" About Me",
+            name: "Bechir Marco"
+        })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+            title:" Help Me",
+            name: "Bechir Marco"
+        })
+})
+
 
 app.get("/weather", (req, res) => {
     res.send({
         Location :"Tunis",
-      Weather :"Tunisia"
-  });
+        Weather :"Tunisia"
+    });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render("error", {
+      title: "404",
+      msg: "Help Article not found",
+      name: "Bechir Marco"
+    });
+})
+app.get('*', (req,res) => {
+    res.render("error", {
+       title: 'Error',
+     msg: "Page Not Found",
+   });
+})
+    
 
 app.listen(3000, () => {
     

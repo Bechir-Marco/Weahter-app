82048ca6878135c621c85f41503b5ac8b352eed3
Bechir-Marco <bechir.marko@gmail.com>
2023-02-16 12:17:31 +0100

    d

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,7 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 
 //Define paths for Exprexx config
 const publicDirectoryPath = path.join(__dirname, "../public");
